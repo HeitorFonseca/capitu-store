701fcb0b736099b82a1f134b01a67e0b82541dc6
Heitor <heitorfonseca.araujo@gmail.com>
2018-08-19 16:57:48 -0300

    dasdasdasda

--- app.js
+++ app.js
@@ -37,21 +37,21 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '\\client\\dist\\index.html'));
 });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.send(err.status);
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.send(err.status);
+// });
 
 mongoose.connect('mongodb://localhost/Capitu', { promiseLibrary: require('bluebird') })
   .then(() =>  console.log('connection successful'))

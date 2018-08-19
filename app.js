b35152547c72447a951a08cf0d52c4d1bff8f88b
Heitor <heitorfonseca.araujo@gmail.com>
2018-08-19 17:15:23 -0300

    asdsadoasdoaid

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(cors());
-app.use(express.static(path.join(__dirname, '/client')));
-app.use('/', express.static(path.join(__dirname, '/client')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use('/', express.static(path.join(__dirname, '/client/dist')));
 
 app.use('/api/product', productRouter);
 app.use('/api/order', orderRouter);
@@ -37,25 +37,25 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '\\client\\dist\\index.html'));
 });
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.send(err.status);
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.send(err.status);
+});
 
-mongoose.connect('mongodb://localhost/Capitu', { promiseLibrary: require('bluebird') })
-  .then(() =>  console.log('connection successful'))
-  .catch((err) => console.error(err));
+// mongoose.connect('mongodb://localhost/Capitu', { promiseLibrary: require('bluebird') })
+//   .then(() =>  console.log('connection successful'))
+//   .catch((err) => console.error(err));
 
 
 module.exports = app;
